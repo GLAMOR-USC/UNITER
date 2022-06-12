1d2b0fbd03adaa4553a7b8fb3f0f324bcafae10e
leticiapinto <leticiapintoalva@gmail.com>
2022-06-11 18:05:54 -0700

    Changing loader file to work with CLiMB

--- data/loader.py
+++ data/loader.py
@@ -11,8 +11,8 @@ import random
 import torch
 from torch.utils.data import DataLoader
 
-from utils.distributed import any_broadcast
-
+#from utils.distributed import any_broadcast
+from ..utils.distributed import any_broadcast ### To work with CLiMB
 
 class MetaLoader(object):
     """ wraps multiple data loaders """
